d0d23e8941cfafe96d551bcc19da2252df965829
Ivan Tavares <ivan.civil93@gmail.com>
2026-04-14 17:52:27 -0300

    Revert "perf(fonts): auto-hospeda Playfair/Inter/Montserrat via @fontsource"
    This reverts commit e90838637fe8672c7927011c022b3a473bfc62ac.

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-// Fontes auto-hospedadas (elimina round-trip para fonts.googleapis.com / fonts.gstatic.com)
-import "@fontsource/playfair-display/600.css";
-import "@fontsource/playfair-display/700.css";
-import "@fontsource/inter/400.css";
-import "@fontsource/inter/500.css";
-import "@fontsource/inter/600.css";
-import "@fontsource/montserrat/500.css";
-import "@fontsource/montserrat/600.css";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
